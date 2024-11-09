72e7a129429f2d384d38d72d1d07a9d69a68595a
amrita123sabat <amritasabat04@gmail.com>
2024-11-09 22:03:06 +0100

    Update projects.html and remove script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Toggle navigation menu
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('nav ul');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-    hamburger.classList.toggle('active');
-});
